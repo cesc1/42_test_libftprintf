e233e889c26388f913eaebbeed632116e11c3215
Francesc Aguirre Plans <faguirre@cbr4s2.42barcelona.com>
2024-10-16 09:55:14 +0200

    add some tests to test_bonus

--- test_bonus.c
+++ test_bonus.c
@@ -6,7 +6,7 @@
 /*   By: faguirre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 14:57:52 by faguirre          #+#    #+#             */
-/*   Updated: 2024/10/10 15:32:51 by faguirre         ###   ########.fr       */
+/*   Updated: 2024/10/16 09:54:09 by faguirre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,9 @@ int	main()
 	FUN("(%d)\n", FUN("1 .%#s.", "hola"));
 	FUN("(%d)\n", FUN("2 .% s.", "hola"));
 	FUN("(%d)\n", FUN("3 .%+s.", "hola"));
+	FUN("(%d)\n", FUN("4 .%5s.", "hola"));
+	FUN("(%d)\n", FUN("4 .%4s.", "hola"));
+	FUN("(%d)\n", FUN("4 .%3s.", "hola"));
 	FUN("(%d)\n", FUN("4 .%05s.", "hola"));
 	FUN("(%d)\n", FUN("5 .%0s.", "hola"));
 	FUN("(%d)\n", FUN("6 .%-s.", "hola"));
@@ -60,6 +63,7 @@ int	main()
 	FUN("(%d)\n", FUN("11 .%#0- +5.5s.", "hola"));
 	//interesting flag combinations
 	FUN("(%d)\n", FUN("12 .%-5s.", "hola"));
+	FUN("(%d)\n", FUN("12 .%-4s.", "hola"));
 	FUN("(%d)\n", FUN("13 .%-05s.", "hola"));
 	FUN("(%d)\n", FUN("14 .%5.5s.", "hola"));
 	FUN("(%d)\n", FUN("15 .%5.7s.", "hola"));
@@ -77,6 +81,12 @@ int	main()
 	FUN("(%d)\n", FUN("6 .%+d.", -5));
 	FUN("(%d)\n", FUN("7 .%+d.", 0));
 	FUN("(%d)\n", FUN("8 .%5i.", 5));
+	FUN("(%d)\n", FUN("8 .%5i.", 555));
+	FUN("(%d)\n", FUN("8 .%3i.", 555));
+	FUN("(%d)\n", FUN("8 .%1i.", 0));
+	FUN("(%d)\n", FUN("8 .%2i.", 0));
+	FUN("(%d)\n", FUN("8 .%01i.", 0));
+	FUN("(%d)\n", FUN("8 .%02i.", 0));
 	FUN("(%d)\n", FUN("9 .%0d.", 5));
 	FUN("(%d)\n", FUN("10 .%-i.", 5));
 	FUN("(%d)\n", FUN("11 .%.d.", 5));
@@ -85,9 +95,12 @@ int	main()
 	FUN("(%d)\n", FUN("13 .%#0- +5.5d.", 5));
 	//interesting flag combinations
 	FUN("(%d)\n", FUN("14 .%-5d.", 5));
-	FUN("(%d)\n", FUN("15 .%-05i.", 5));
+	FUN("(%d)\n", FUN("15 .%-05i.", 55));
+	FUN("(%d)\n", FUN("15 .%-01i.", 55));
 	FUN("(%d)\n", FUN("16 .%5.5d.", 5));
-	FUN("(%d)\n", FUN("17 .%5.7i.", 5));
+	FUN("(%d)\n", FUN("17 .%5.7i.", 55555));
+	FUN("(%d)\n", FUN("17 .%5.3i.", 55555));
+	FUN("(%d)\n", FUN("17 .%5.5i.", 55555));
 	FUN("(%d)\n", FUN("18 .%-05.7d.", 5));
 	FUN("(%d)\n", FUN("19 .% +d.", 5));
 	FUN("(%d)\n", FUN("20 .% +d.", -5));
@@ -98,10 +111,15 @@ int	main()
 	FUN("(%d)\n", FUN("1 .%#u.", 5));
 	FUN("(%d)\n", FUN("2 .% u.", 5));
 	FUN("(%d)\n", FUN("3 .%+u.", 5));
-	FUN("(%d)\n", FUN("4 .%5u.", 5));
-	FUN("(%d)\n", FUN("5 .%0u.", 5));
-	FUN("(%d)\n", FUN("6 .%-u.", 5));
+	FUN("(%d)\n", FUN("4 .%5u.", 555));
+	FUN("(%d)\n", FUN("4 .%2u.", 555));
+	FUN("(%d)\n", FUN("4 .%3u.", 555));
+	FUN("(%d)\n", FUN("5 .%05u.", 5));
+	FUN("(%d)\n", FUN("5 .%01u.", 5));
+	FUN("(%d)\n", FUN("6 .%-3u.", 555));
+	FUN("(%d)\n", FUN("6 .%-5u.", 555));
 	FUN("(%d)\n", FUN("7 .%.u.", 5));
+	FUN("(%d)\n", FUN("7 .%.3u.", 5));
 	//all flags
 	FUN("(%d)\n", FUN("8 .%#0- +5.5u.", 5));
 	//interesting flag combinations
@@ -117,7 +135,10 @@ int	main()
 	FUN("(%d)\n", FUN("1 .%#x.", 13));
 	FUN("(%d)\n", FUN("2 .% X.", 13));
 	FUN("(%d)\n", FUN("3 .%+x.", 13));
-	FUN("(%d)\n", FUN("4 .%5X.", 13));
+	FUN("(%d)\n", FUN("4 .%1X.", 1000));
+	FUN("(%d)\n", FUN("4 .%5X.", 1000));
+	FUN("(%d)\n", FUN("4 .%01X.", 1000));
+	FUN("(%d)\n", FUN("4 .%01X.", 1000));
 	FUN("(%d)\n", FUN("5 .%0x.", 13));
 	FUN("(%d)\n", FUN("6 .%-X.", 13));
 	FUN("(%d)\n", FUN("7 .%.x.", 13));
@@ -140,6 +161,10 @@ int	main()
 	FUN("(%d)\n", FUN("2 .% p.", ptr));
 	FUN("(%d)\n", FUN("3 .%+p.", ptr));
 	FUN("(%d)\n", FUN("4 .%5p.", ptr));
+	FUN("(%d)\n", FUN("4 .%20p.", ptr));
+	FUN("(%d)\n", FUN("4 .%-20p.", ptr));
+	FUN("(%d)\n", FUN("4 .%020p.", ptr));
+	FUN("(%d)\n", FUN("4 .%-020p.", ptr));
 	FUN("(%d)\n", FUN("5 .%0p.", ptr));
 	FUN("(%d)\n", FUN("6 .%-p.", ptr));
 	FUN("(%d)\n", FUN("7 .%.p.", ptr));
